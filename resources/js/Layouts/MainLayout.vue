4f45ee63fd81fe7db0b9d275c518c4a6f4c020a6
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-08-24 15:30:19 +0200

    navbar

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <div id="MainLayout" 
+        class="w-full h-screen">
+
+        <div v-if="$page.url === '/' " id="TopNavHome" class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
+            <div class="flex items-center justify-between h-full">
+
+                <Link href="/">
+                    <img class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png"/>
+                </Link>
+
+                <div class="flex items-center w-[50%]">
+                    
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <slot />
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+
+</script>
+
+<template>
+    <Head title="HOME" />
+    
+    <MainLayout>
+        
+    </MainLayout>
+</template>
+
+<style>
+
+</style>
