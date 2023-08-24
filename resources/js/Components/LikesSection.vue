f8230ec932d738be7bb8081fe5de7d83a9de2c62
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-08-25 00:40:36 +0200

    Some modifications

---
+++ resources/js/Components/LikesSection.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed, toRefs } from 'vue'
+import { usePage } from '@inertiajs/vue3'
+
+import Heart from 'vue-material-design-icons/Heart.vue'
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
+import CommentOutline from 'vue-material-design-icons/CommentOutline.vue'
+import SendOutline from 'vue-material-design-icons/SendOutline.vue'
+import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue'
+</script>
+
+
+<template>
+    <div class="flex z-20 items-center justify-between">
+        <div class="flex items-center">
+            <button class="-mt-[14px]">
+                <HeartOutline class="pl-3 cursor-pointer" :size="30"/>
+            </button>
+        </div>
+    </div>
+
+</template>
+
+
+<style>
+
+    </style>
---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive, toRefs } from 'vue'
+import { Head, Link, router } from '@inertiajs/vue3'
+
+import MainLayout from '@/Layouts/MainLayout.vue'
+import ShowPostOverlay from '@/Components/ShowPostOverlay.vue'
+import ContentOverlay from '@/Components/ContentOverlay.vue'
+
+import Cog from 'vue-material-design-icons/Cog.vue'
+import Grid from 'vue-material-design-icons/Grid.vue'
+import PlayBoxOutline from 'vue-material-design-icons/PlayBoxOutline.vue'
+import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue'
+import AccountBoxOutline from 'vue-material-design-icons/AccountBoxOutline.vue'
+
+
+let data = reactive({post : null})
+const form = reactive({file: null})
+
+const props = defineProps()
+
+</script>
+
+<template>
+    <Head title="Instagram" />
+    
+    
+</template>
+
+
