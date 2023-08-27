fb00a0270276c70d3b4cdbb4ab2d1a9ea8436b41
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-08-27 16:02:47 +0200

    Connect database and backend

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'file' => '/user-placeholder.png',
             'password' => Hash::make($request->password),
         ]);
 
